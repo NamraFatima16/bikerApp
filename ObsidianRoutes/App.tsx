e49c05d3bd93777733c4718ba23abf587318eb5a
Namra Fatima <20094326@mail.wit.ie>
2026-03-19 13:50:46 +0000

    feat: add navigation shell (#6)

--- ObsidianRoutes/App.tsx
+++ ObsidianRoutes/App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from './screens/auth/LoginScreen'
 import SignupScreen from "./screens/auth/SignupScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppNavigator from './navigation/AppNavigator';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,8 @@ export default function App(){
       <Stack.Navigator initialRouteName = "Login" screenOptions={{ headerShown:false}}>
         <Stack.Screen name = "Login" component = {LoginScreen}/>
         <Stack.Screen name = "Signup" component = {SignupScreen}/>
+        <Stack.Screen name = "App" component = {AppNavigator}/>
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
