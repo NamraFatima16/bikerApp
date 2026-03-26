68e5a94daba27d8433fc9e8bc67a267e2e557bc9
Namra Fatima <20094326@mail.wit.ie>
2026-03-26 16:59:06 +0000

    chore: fix prettier formatting

--- ObsidianRoutes/App.tsx
+++ ObsidianRoutes/App.tsx
@@ -1,18 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
-import LoginScreen from './screens/auth/LoginScreen';
+import LoginScreen from "./screens/auth/LoginScreen";
 import SignupScreen from "./screens/auth/SignupScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import AppNavigator from './navigation/AppNavigator';
+import AppNavigator from "./navigation/AppNavigator";
 
 const Stack = createNativeStackNavigator();
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen}/>
-        <Stack.Screen name="Signup" component={SignupScreen}/>
-        <Stack.Screen name="App" component={AppNavigator}/>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="App" component={AppNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
