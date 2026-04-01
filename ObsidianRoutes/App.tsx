30c818a8c05e7c86711c919dfd95649ec44e2314
Namra Fatima <20094326@mail.wit.ie>
2026-04-01 11:29:58 +0100

    chore: fix prettier formatting

--- ObsidianRoutes/App.tsx
+++ ObsidianRoutes/App.tsx
@@ -3,10 +3,10 @@ import LoginScreen from "./screens/auth/LoginScreen";
 import SignupScreen from "./screens/auth/SignupScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AppNavigator from "./navigation/AppNavigator";
-import * as Sentry from '@sentry/react-native';
+import * as Sentry from "@sentry/react-native";
 
 Sentry.init({
-  dsn: 'https://be4aac2db75666082e782a5361dbfbd4@o4511111990411264.ingest.de.sentry.io/4511113762439248',
+  dsn: "https://be4aac2db75666082e782a5361dbfbd4@o4511111990411264.ingest.de.sentry.io/4511113762439248",
 
   // Adds more context data to events (IP address, cookies, user, etc.)
   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
@@ -18,7 +18,10 @@ Sentry.init({
   // Configure Session Replay
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: __DEV__,
