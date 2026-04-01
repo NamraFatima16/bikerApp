d318b62b79233ce5c52579873a86d4259da09780
Namra Fatima <20094326@mail.wit.ie>
2026-04-01 11:24:25 +0100

    chore: add Sentry monitoring (#8)

--- ObsidianRoutes/App.tsx
+++ ObsidianRoutes/App.tsx
@@ -3,10 +3,30 @@ import LoginScreen from "./screens/auth/LoginScreen";
 import SignupScreen from "./screens/auth/SignupScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AppNavigator from "./navigation/AppNavigator";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://be4aac2db75666082e782a5361dbfbd4@o4511111990411264.ingest.de.sentry.io/4511113762439248',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+export default Sentry.wrap(function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -19,4 +39,4 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+});
