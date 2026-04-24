72243b86d30ffa1a66214eb0bc1bf22663fe6fb7
Namra Fatima <20094326@mail.wit.ie>
2026-04-24 14:35:32 +0100

    chore: fix prettier formatting

--- ObsidianRoutes/lib/utils/network.ts
+++ ObsidianRoutes/lib/utils/network.ts
@@ -1,12 +1,12 @@
 import NetInfo from "@react-native-community/netinfo";
 
 export async function isConnected(): Promise<boolean> {
-    const state = await NetInfo.fetch();
-    return state.isConnected === true;
+  const state = await NetInfo.fetch();
+  return state.isConnected === true;
 }
 
 export function useNetworkStatus(callback: (connected: boolean) => void) {
-    return NetInfo.addEventListener((state) => {
-        callback(state.isConnected === true);
-    });
+  return NetInfo.addEventListener((state) => {
+    callback(state.isConnected === true);
+  });
 }
